8f36af0d15f72d5f51387da54478b905a34ebc5e
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-27 19:10:32 -0700

    main_V1.sh
    An archive of the first version of the main script for this project.

---
+++ Automation/OldVersions/Shell/1/1-100/main_V1.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Start of script
+# The main automation script for SNU Flags Of The World
+# This file is heavily incomplete, and is currently pseudocode/bad code, as I do not know automation in Shell yet, or Shell in general at a high level.
+function main() {
+  # The whole process, returns all functions and runs them
+  return automateMake()
+  return getFlagsGIF()
+  return getFlagsPNG()
+  return getFlagsSVG()
+  return getSiteData()
+  return sorting()
+  break
+}
+function getFlagsGIF() {
+  # Retrieves flag images in GIF format from a source
+  break
+}
+function getFlagsPNG() {
+  # Retrieves flag images in PNG format from a source
+  break
+}
+function getFlagsSVG() {
+  # Retrieves flag images in SVG format from a source
+  break
+}
+function automateMake() {
+  # Executes the makefile
+  return $/makefile
+  break
+}
+function sorting() {
+  # Sorts the files into a working structure
+  break
+}
+return main() # Runs the main command and begins automation
+
+# File info
+# File type: Shell script file (*.sh)
+# File version: 1 (Sunday, June 27th 2021 at 7:10 pm)
+# Line count (including blank lines and compiler line): 44
+
+# End of script
